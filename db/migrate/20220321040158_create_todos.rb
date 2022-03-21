89eb31fc6e5476b733560c3da0f542501dc14f89
fukumotomasashi <cvb25362@gmail.com>
2022-03-21 15:05:41 +0900

    create todo index

--- db/migrate/20220321040158_create_todos.rb
+++ db/migrate/20220321040158_create_todos.rb
@@ -3,7 +3,7 @@ class CreateTodos < ActiveRecord::Migration[7.0]
     create_table :todos do |t|
       t.string :title, null: false
       t.text :detail
-      t.boolean :delete_flg
+      t.boolean :delete_flg, default: false
       t.timestamps
     end
   end
