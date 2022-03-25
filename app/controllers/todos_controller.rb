22e32da93931c2739224d3811c77403c1ebb3743
fukumotomasashi <cvb25362@gmail.com>
2022-03-25 21:06:26 +0900

    createの作成

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,17 @@
 class TodosController < ApplicationController
   def index
+    @todo = Todo.new
     @todos = Todo.all
+  end
+
+  def create
+    Todo.create(task: params_permitted[:task])
+    redirect_to root_path
+  end
+
+  private
+
+  def params_permitted
+    params.require(:todo).permit(:task)
   end
 end
